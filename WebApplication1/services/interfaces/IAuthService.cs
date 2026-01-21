005d5f1deb1ef218e8607c0cef69154bb179462e
dung382 <dinhvietdung3003thd@gmail.com>
2026-01-21 16:29:49 +0700

    them elasticsreach cho phan tim kiem mon an, them phan quyen cho vc them, sua, xoa cac mon an

--- WebApplication1/services/interfaces/IAuthService.cs
+++ WebApplication1/services/interfaces/IAuthService.cs
@@ -4,7 +4,7 @@ namespace WebApplication1.services.interfaces
 {
     public interface IAuthService
     {
-        Task<AccountDTO?> LoginAsync(LoginRequest request);
+        Task<object?> LoginAsync(LoginRequest request);
         Task<int> RegisterAsync( RegisterRequest request);
 
         Task<CustomerAccountDTO?> CustomerLoginAsync(LoginRequest request);
