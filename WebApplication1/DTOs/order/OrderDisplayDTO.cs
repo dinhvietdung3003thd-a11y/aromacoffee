dd1881bbff5c263f3dd589eec18d2c637d11a300
dung382 <dinhvietdung3003thd@gmail.com>
2026-03-04 21:14:47 +0700

    khoa phan ship

--- WebApplication1/DTOs/order/OrderDisplayDTO.cs
+++ WebApplication1/DTOs/order/OrderDisplayDTO.cs
@@ -9,11 +9,11 @@
         public string? Status { get; set; }
         public string? CreatorFullName { get; set; }
         public int? UserId { get; set; }
-        public string? ShippingAddress { get; set; }
-        public double? Lat { get; set; }
-        public double? Lng { get; set; }
-        public double? DistanceKm { get; set; }
-        public decimal? ShippingFee { get; set; }
+        //public string? ShippingAddress { get; set; }
+        //public double? Lat { get; set; }
+        //public double? Lng { get; set; }
+        //public double? DistanceKm { get; set; }
+        //public decimal? ShippingFee { get; set; }
         public List<OrderDetailDTO> Details { get; set; } = new List<OrderDetailDTO>();
     }
 }
