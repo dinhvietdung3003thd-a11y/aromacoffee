ac184c6fd9bc8d8a4fbb393549bb30c7d9ee9eb6
dung382 <dinhvietdung3003thd@gmail.com>
2026-03-24 15:32:03 +0700

    xoa file rác

--- IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApplication1.DTOs;
-
-namespace WebApplication1.Services.Interfaces
-{
-    public interface IAuthService
-    {
-        Task<AccountDTO?> LoginAsync(LoginRequest request);
-    }
-}
