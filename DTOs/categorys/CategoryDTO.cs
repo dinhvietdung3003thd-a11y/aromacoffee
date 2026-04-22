476b01ec4f2b3882d11cea37d586868ea58d5dc7
dinhvietdung3003thd-a11y <dinhvietdung3003thd@gmail.com>
2026-04-22 14:45:56 +0700

    Revert "Main"

--- DTOs/categorys/CategoryDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.DTOs.categorys
-{
-    public class CategoryDTO
-    {
-        public int CategoryId { get; set; }          
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }   
-}
--- models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; } // Auto increment
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }
-}
